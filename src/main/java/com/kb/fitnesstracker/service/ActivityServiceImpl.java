49d3a995277738ece94a4911fbab54d812f2860a
Kyle Bennett <kyle@orangebus.co.uk>
2019-11-24 21:02:19 +0000

    Logging config

--- src/main/java/com/kb/fitnesstracker/service/ActivityServiceImpl.java
+++ src/main/java/com/kb/fitnesstracker/service/ActivityServiceImpl.java
@@ -2,22 +2,21 @@ package com.kb.fitnesstracker.service;
 
 import com.kb.fitnesstracker.dao.MongoActivityRepository;
 import com.kb.fitnesstracker.model.Activity;
-import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 
+@Slf4j
 @Service
-@Log4j
 public class ActivityServiceImpl implements ActivityService {
 
     @Autowired
     MongoActivityRepository repo;
 
     public Collection<Activity> listAllActivities() {
-
-        log.debug("List All Activities");
+        log.debug("Getting all Activities");
         return repo.findAll();
     }
 
